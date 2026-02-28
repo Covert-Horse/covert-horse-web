b7203c700ddc31c454576adae3e71e6ab5d3e2bc
Jake Grdic <grdic.5@osu.edu>
2026-02-27 23:33:34 -0500

    adds product catalog feature

---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default {
+    products: [{
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Slim Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 75,
+        brand: 'Adidas',
+        rating: 4.0,
+        numberOfReviews: 8,
+    },
+    {
+        name: 'Classic Tee',
+        imageUrl: '/images/d1.jpg',
+        price: 35,
+        brand: 'H&M',
+        rating: 3.8,
+        numberOfReviews: 15,
+    }]
+}
